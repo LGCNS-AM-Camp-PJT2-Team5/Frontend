e052057f6f3efb093701abadb0f85d7b9d2cad2e
cjh-19 <gns8812@naver.com>
2025-02-27 05:17:10 +0900

    [Feat #32] 마이페이지 - 내 정보 기능

--- src/pages/mypage/MyPage.jsx
+++ src/pages/mypage/MyPage.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
-//import UserInfoContainer from "../../components/mypage/UserInfoContainer";
 import InterestCompanies from "./interest_company/InterestCompanies";
 import ActiveTabBar from "../../components/admin/ActiveTabBar";
+import Profile from "./profiles/Profile";
 
 /**
  * 마이페이지 컴포넌트 (회원 정보 조회/수정 & 관심 기업 선택)
  */
 export default function MyPage() {
-    const [selectedTab, setSelectedTab] = useState("userInfo");
+    const [selectedTab, setSelectedTab] = useState("profile");
 
     const tabs = [
-        { id: "userInfo", label: "회원 정보 조회/수정" },
+        { id: "profile", label: "회원 정보 조회/수정" },
         { id: "interestCompanies", label: "관심 기업 선택" }
     ];
 
@@ -20,7 +20,7 @@ export default function MyPage() {
             <ActiveTabBar tabs={tabs} onSelect={setSelectedTab} />
 
             {/* 선택된 탭에 따라 컴포넌트 렌더링 */}
-            {/*{selectedTab === "userInfo" && <UserInfoContainer />}*/}
+            {selectedTab === "profile" && <Profile />}
             {selectedTab === "interestCompanies" && <InterestCompanies />}
         </div>
     );
